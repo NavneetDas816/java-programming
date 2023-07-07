e55532f34540ca01eee90dc097d56eeb1ebfd553
NAVNEET DAS <138598592+NavneetDas816@users.noreply.github.com>
2023-07-07 17:33:23 +0530

    Add files via upload

---
+++ Addition/Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+	public static void main(String numbers[])
+	{
+		int num_1=Integer.parseInt(numbers[0]);
+		int num_2=Integer.parseInt(numbers[1]);
+		int c=num_1+num_2;
+		System.out.println(c);
+	}
+}
